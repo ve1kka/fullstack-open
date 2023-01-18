c9c0aa5d630327fc9f133df38040a6971bb30eb9
ve1kka <veikka.salmela@gmail.com>
2023-01-18 15:39:55 +0200

    2.2 and 2.3

--- part2/courseinfo/src/App.js
+++ part2/courseinfo/src/App.js
@@ -1,56 +1,66 @@
 const Header = (props) => {
     return (<h1>{props.name}</h1>)
-  }
-  
-  const Part = (props) => {
+}
+
+const Part = (props) => {
     return (<p>{props.name} {props.exercises}</p>)
-  }
-  
-  const Content = (props) => {
+}
+
+const Content = (props) => {
     return (
-      props.parts.map(c => <Part key={c.id} name={c.name} exercises={c.exercises}/>)
+        props.parts.map(c => <Part key={c.id} name={c.name} exercises={c.exercises}/>)
     )
-  }
+}
+
+
+const Total = (props) => {
+    return (
+        <p><b>total of {props.parts.map(c => c.exercises).reduce((total, curr) => { return total + curr })} exercises</b></p>
+    )
+}
   
-  const Course = (props) => {
+const Course = (props) => {
     return(
-      <div>
-        <Header name={props.course.name}/>
-        <Content parts={props.course.parts}/>
-      </div>
+        <div>
+            <Header name={props.course.name}/>
+            <Content parts={props.course.parts}/>
+            <Total parts={props.course.parts}/>
+        </div>
     )
-  }
+}
+
+
   
   
   
-  const App = () => {
+const App = () => {
     const course = {
-      name: 'Half Stack application development',
-      id: 1,
-      parts: [
+        name: 'Half Stack application development',
+        id: 1,
+        parts: [
         {
-          name: 'Fundamentals of React',
-          exercises: 10,
-          id: 1
+            name: 'Fundamentals of React',
+            exercises: 10,
+            id: 1
         },
         {
-          name: 'Using props to pass data',
-          exercises: 7,
-          id: 2
+            name: 'Using props to pass data',
+            exercises: 7,
+            id: 2
         },
         {
-          name: 'State of a component',
-          exercises: 14,
-          id: 3
+            name: 'State of a component',
+            exercises: 14,
+            id: 3
         }
-      ]
+        ]
     }
-  
+
     return (
-      <div>
+        <div>
         <Course course={course} />
-      </div>
+        </div>
     )
-  }
+}
   
   export default App
