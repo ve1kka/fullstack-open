b5aea4d4102a4f5f1daef34500978986487c2554
ve1kka <veikka.salmela@gmail.com>
2023-02-07 15:10:20 +0200

    4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -5,6 +5,13 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + blog.likes
+  }, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
 }
