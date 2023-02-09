ee04cb1c4e0d5b59255eace32a4387654cab4d46
ve1kka <veikka.salmela@gmail.com>
2023-02-09 19:16:17 +0200

    userstuff

---
+++ part4/controllers/users.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs', {
+    title: 1,
+    likes: 1,
+  })
+  response.json(users)
+})
+
+usersRouter.delete('/:id', async (request, response) => {
+  await User.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+module.exports = usersRouter
